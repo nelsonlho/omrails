dc420a7b0c142616c9964d3c9665754334532119
Nelson Ho <nelsonlho@gmail.com>
2016-11-08 15:51:24 -0800

    added tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
   as :user do
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get '*path', to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
