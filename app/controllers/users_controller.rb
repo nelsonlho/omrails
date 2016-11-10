70967057a9d7e052ec6e8d8897faacb3370ead50
Nelson Ho <nelsonlho@gmail.com>
2016-11-09 19:00:05 -0800

    added user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
+    @tweets = @user.tweets
     redirect_to(root_path, :notice => 'User not found') unless @user
   end
-
-
 end
